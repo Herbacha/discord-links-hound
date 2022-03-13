a7fb97bb29bb84862edf6c57c39a684d89b5f6c7
Pierre-Marie Aubineau <pm@cataire.fr>
2022-03-13 13:29:57 +0100

    🐛 (gen) Fixed links not appearing by date + date format

--- src/generator_formats/generate_list_by_date.py
+++ src/generator_formats/generate_list_by_date.py
@@ -8,15 +8,13 @@ def generate_list_by_date(linkdata_list):
     links_by_date = {}
 
     for linkdata in linkdata_list:
-        current_link_date = linkdata.date.strftime("%m/%d/%Y")
+        current_link_date = linkdata.date.strftime("%d/%m/%Y")
         if current_link_date not in links_by_date:
             links_by_date[current_link_date] = []
         links_by_date[current_link_date].append(linkdata)
 
     for date, links in links_by_date.items():
         output_lines.append(f'# {date}')
-        print({date})
-        print(links)
-        generate_list_by_author(links, '## Posted by ')
+        output_lines.append(generate_list_by_author(links, '## Posted by'))
 
     return '\r'.join(output_lines)
