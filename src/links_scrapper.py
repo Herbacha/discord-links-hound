bf7db743500ed9f85a7ac44f068d5b1dff6c52d3
Pierre-Marie Aubineau <pm@cataire.fr>
2022-03-13 13:02:14 +0100

    🎨(scapper) Separated scrapper logic from generation logic

--- src/links_scrapper.py
+++ src/links_scrapper.py
@@ -1,18 +1,16 @@
-import discord
-from src.output_generator import generate_list
 from src.url_finder import get_links
 from src.linkdata_model import Linkdata
 
 
-async def scrap(message, client, display_in_channel=False):
+async def scrap(message, client):
     retrieved_links = []
     channel = message.channel
     guild = message.guild
-    async for history_message in channel.history():
+    async for history_message in channel.history(limit=10000):
         if history_message.author != client.user:
             results = get_links(history_message.content)
             if results is not None:
                 for matched_url in results:
                     retrieved_links.append(
                         Linkdata(matched_url, guild.get_member(int(history_message.author.id)), history_message.created_at))
-    await generate_list(message, retrieved_links, display_in_channel)
+    return retrieved_links
