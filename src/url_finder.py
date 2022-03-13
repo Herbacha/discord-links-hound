ebe09de471505c22f801842d3fe25537393c9d82
Pierre-Marie Aubineau <pm@cataire.fr>
2022-03-13 13:03:30 +0100

    ✨(scapper) removed tenor.com matches from url_finder

--- src/url_finder.py
+++ src/url_finder.py
@@ -2,7 +2,11 @@ import re
 url_pattern = 'https?:[a-zA-Z0-9_.+-/#~]+'
 
 
-def get_links(message):
+def get_links(message, remove_tenor=True):
     results = re.findall(url_pattern, message)
     if len(results) > 0:
+        if(remove_tenor):
+            for r in results:
+                if 'tenor.com' in r:
+                    results.remove(r)
         return results
